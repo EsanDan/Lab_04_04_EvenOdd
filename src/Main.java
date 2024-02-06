f4bc4b75afdbeaf4292530323769dcbddbf6de60
EsanDan <esanod@mail.uc.edu>
2024-02-05 19:21:43 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int numToExamine = 0;
+        int value = 37;
+
+        if (value % 2 ==numToExamine)
+            System.out.println("Your value is even");
+        else
+            System.out.println("Your value is Odd");
+
+
+    }
+}
